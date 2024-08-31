261ab7ae9eba707e10e6fca9ea2608d486d3e3cd
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-31 09:36:34 +0600

    task 3

--- 3 Grade_Calculator.js
+++ 3 Grade_Calculator.js
@@ -11,3 +11,15 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+var marks = 30;
+if(marks >= 90){
+    console.log("A")
+}else if(marks >= 80 && marks <= 89){
+    console.log("B")
+}else if(marks >= 70 && marks <= 79){
+    console.log("C")
+}else if(marks >= 60 && marks <= 69){
+    console.log("D")
+}else{
+    console.log("F")
+}
