8a81091ba1ec96711a2cc9fe39c1d3383c7cf47d
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-31 10:19:28 +0600

    Task 6

--- 6 bus fare calculator.js
+++ 6 bus fare calculator.js
@@ -6,3 +6,14 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+var age = 10;
+var isStudent = true;
+if(age < 10){
+    console.log("free")
+}else if(age >= 10 && age <=59 && isStudent == true){
+    console.log("50% discount")
+}else if(age >= 60){
+    console.log("15% discount")
+}else{
+    console.log("800 taka")
+}
